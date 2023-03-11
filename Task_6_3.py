2340017b2e150663c6303352374236d6479700d0
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-03-11 20:40:50 +0100

    Tables fill-out

--- Task_6_3.py
+++ Task_6_3.py
@@ -1,9 +1,13 @@
-from sqlalchemy import create_engine, MetaData, Integer, Float, String, Date, Table, Column
+import csv
+from sqlalchemy import create_engine, MetaData, Integer, Float, String, Table, Column
 
-engine = create_engine('sqlite:///C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3.dtabase.db', echo=True)
+engine = create_engine('sqlite:///C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3_database.db', echo=True)
+
+database="C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3_database.db"
 
 meta = MetaData()
 
+# Tables
 stations = Table(
    'stations', meta,
    Column('id', Integer, primary_key=True),
@@ -20,10 +24,31 @@ measures = Table(
    'measures', meta,
    Column('id', Integer, primary_key=True),
    Column('station', String),
-   Column('date', Date),
+   Column('date', String),
    Column('precip', Float),
    Column('tobs', Integer),   
 )
 
 meta.create_all(engine)
-print(engine.table_names())
+
+# Tables fill-out
+stations_records="C:/Users/user/Downloads/clean_stations.csv"
+table_stations = Table(stations, meta, autoload=True, autoload_with=engine)
+measures_records="C:/Users/user/Downloads/clean_measure.csv"
+table_measures = Table(measures, meta, autoload=True, autoload_with=engine)
+
+with open(stations_records, 'r') as f:
+    csv_reader = csv.reader(f)
+    headers = next(csv_reader)
+    data = [dict(zip(headers, row)) for row in csv_reader]
+
+with engine.connect() as conn:
+    conn.execute(table_stations.insert(), data)
+
+with open(measures_records, 'r') as f:
+    csv_reader = csv.reader(f)
+    headers = next(csv_reader)
+    data = [dict(zip(headers, row)) for row in csv_reader]
+
+with engine.connect() as conn:
+    conn.execute(table_measures.insert(), data)
