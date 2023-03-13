acbfa8d5e9c5b9dac1bdde618b5e2eaf3e20e234
Alicja Popielarska <popielarska.alicja@gmail.com>
2023-03-13 17:32:11 +0100

    Final code

--- Task_6_3.py
+++ Task_6_3.py
@@ -1,7 +1,7 @@
 import csv
-from sqlalchemy import create_engine, MetaData, Integer, Float, String, Table, Column
+from sqlalchemy import create_engine, MetaData, Integer, Float, String, Table, Column, ForeignKey
 
-engine = create_engine('sqlite:///C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3_database.db', echo=True)
+engine = create_engine('sqlite:///C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3_database.db', echo=False)
 
 database="C:/Users/user/OneDrive/Desktop/Kodilla/6_3/Task_6_3_database.db"
 
@@ -23,13 +23,15 @@ stations = Table(
 measures = Table(
    'measures', meta,
    Column('id', Integer, primary_key=True),
-   Column('station', String, foreign_key="stations.station", nullable=False ),
+   Column('station', String, ForeignKey("stations.station"), nullable=False ),
    Column('date', String),
    Column('precip', Float),
    Column('tobs', Integer),   
 )
 
 meta.create_all(engine)
+
+conn=engine.connect()
 
 # Tables fill-out
 stations_records="C:/Users/user/Downloads/clean_stations.csv"
@@ -39,14 +41,19 @@ with open(stations_records, 'r') as f:
     csv_reader = csv.reader(f)
     headers = next(csv_reader)
     records = [dict(zip(headers, row)) for row in csv_reader]
-
-with engine.connect() as conn:
     conn.execute(stations.insert(), records)
 
 with open(measures_records, 'r') as f:
     csv_reader = csv.reader(f)
     headers = next(csv_reader)
     records = [dict(zip(headers, row)) for row in csv_reader]
-
-with engine.connect() as conn:
     conn.execute(measures.insert(), records)
+
+# Example from the excercise
+print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+
+
+
+
+# Closing connection
+conn.close()
